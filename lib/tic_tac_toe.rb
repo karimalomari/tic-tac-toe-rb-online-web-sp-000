c3a39ad856f874228906999f1a11cc02503e709e
Karim Alomari <karim.alomari@gmail.com>
2020-07-14 12:31:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,3 +48,13 @@ def turn(board)
   end
   display_board(board)
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |index|
+    if index != " "
+      count += 1
+    end
+  end
+    count
+ end
