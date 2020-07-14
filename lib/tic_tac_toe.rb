ff7796100757ae12bd5dda4336ab072d0f22851b
Karim Alomari <karim.alomari@gmail.com>
2020-07-14 12:28:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,3 +32,17 @@ def position_taken? (board, index)
       taken = true
   end
 end
+
+def valid_move?(board, index)
+  if board[index] == "X" || board[index] == "O"&& index.between?(0, 8)
+    return false 
+
+  elsif 
+    board[index] == " "||board[index] == ""|| board[index] == nil && index.between?(0, 8)
+    return true 
+
+  else 
+    return false
+
+ end
+end
