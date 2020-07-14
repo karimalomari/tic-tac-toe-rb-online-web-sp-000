a67953c5aedc4366706b6856ec7adaa235c79df6
Karim Alomari <karim.alomari@gmail.com>
2020-07-14 12:50:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -124,7 +124,15 @@ def winner (board)
 end
 
 def play(board)
-  while !over?(board) && !won?(board) && !draw?(board)
+  until over?(board) == true || won?(board) != false
+  puts 'turn'
     turn(board)
+  end
+  if winner(board)
+    puts "Congratulations!"
+  elsif draw?(board)
+    puts "Draw!"
+  else
+    return nil
   end
 end
